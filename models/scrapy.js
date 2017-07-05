f6a3a586c22bc44b667edf8203c6b8fc76337a70
David_X <nowindxdw@126.com>
2017-07-05 19:09:13 +0800

    Update scrapy.js

--- models/scrapy.js
+++ models/scrapy.js
@@ -10,7 +10,8 @@ var async = require('async');
 var _ = require('lodash');
 var Logger = require("logger-romens");
 var logger = new Logger();
-
+var moment = require('moment');
+var snowflakeId = require('snowflakeid-romens').getSnowflakeIDFactory(function(){});
 var TIMEFORMAT_REG = /([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))/
 module.exports = function () {
     var model = {
@@ -50,19 +51,30 @@ module.exports = function () {
             var locationEle = $('#job_detail > dd.job-address.clearfix > input[type="hidden"]:nth-child(6)');
             var jobDescEle = $('#job_detail > dd.job_bt > div');
             var requireExpEle = $('body > div.position-head > div > div.position-content-l > dd > p:nth-child(1) > span:nth-child(3)');
-
-            var recruitObj = {
-                enterpriseName : companyEle.text(),
-                startTime:publishTimeEle.text().match(TIMEFORMAT_REG)[0],
-                jobTitle : jobNameEle.text(),
-                salary : salaryEle.text(),
-                location:locationEle.attr('value'),
-                jobDesc:jobDescEle.text(),
-                requireExp: requireExpEle.text(),
-                languageType:language.toUpperCase()
-
-            };
-            logger.debug(recruitObj);
+            var startTime = publishTimeEle.text().match(TIMEFORMAT_REG);
+            logger.trace(jobNameEle.text());
+            if(_.isNull(startTime)){
+                startTime = moment().format("YYYY-MM-DD HH:mm:ss");
+            }else{
+                startTime = publishTimeEle.text().match(TIMEFORMAT_REG)[0];
+            }
+            var recruitObj = {};
+            if(!_.isEmpty(jobNameEle.text())){
+                recruitObj={
+                    recruitSfId : snowflakeId.next().toString(),
+                    enterpriseName : companyEle.text(),
+                    startTime:startTime,
+                    jobTitle : jobNameEle.text(),
+                    salary : salaryEle.text(),
+                    location:locationEle.attr('value'),
+                    jobDesc:jobDescEle.text(),
+                    requireExp: requireExpEle.text(),
+                    languageType:language.toUpperCase(),
+                    createdAt:moment().format("YYYY-MM-DD HH:mm:ss"),
+                    updatedAt:moment().format("YYYY-MM-DD HH:mm:ss")
+                };
+                logger.debug(recruitObj);
+            }
             var similarList = $('#jobs_similar_detail > ul li');
             var similarUrls = [];
             _.each(similarList,function(item){
@@ -70,7 +82,7 @@ module.exports = function () {
                 var jobId = $element.attr('data-jobid');
                 similarUrls.push('https://www.lagou.com/jobs/'+jobId+'.html');
             });
-            logger.trace(similarUrls);
+            logger.trace('similarUrls',similarUrls);
             callback(null,{recruitObj:recruitObj,similarUrls:similarUrls})
         },
     };
