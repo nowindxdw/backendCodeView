846467ab31ee2cf02157035f591a2e2a4b8477dc
nowindxdw <nowindxdw@126.com>
2017-07-28 11:05:46 +0800

    update swagger and controller

---
+++ swagger/v1/controllers/DashService.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.getDashboard = function(args, res, next) {
+  /**
+   * 后台默认页面渲染
+   * 获取后台首页
+   *
+   * no response value expected for this operation
+   **/
+  res.end();
+}
+
