e92a9874f49c1e95db0fef9bd2fb67fae23c5acd
nowindxdw <nowindxdw@126.com>
2017-08-01 09:30:08 +0800

    add logout ajax

---
+++ public/js/logout.js
@@ -0,0 +1,25 @@
+function btnLogout() {
+    var username = localStorage.operatorName;
+    var token = localStorage.token;
+    var url = "/v1/api/auth/"+username;
+    var href = window.location.href.split("/v1/")[0];
+    //向后台发送处理数据
+    $.ajax({
+        type: "DELETE", //用DELETE方式传输
+        dataType: "json", //数据格式:JSON
+        contentType: "application/json",
+        headers:{
+            "Authorization":token
+        },
+        async:false,
+        url: url, //目标地址
+        complete: function (XMLHttpRequest, textStatus, errorThrown) {
+            if(XMLHttpRequest.status == 200){
+                localStorage.operatorName = "";
+                localStorage.token = "";
+                window.location.href=href+"/v1/api/login";
+            }
+            alert(XMLHttpRequest.responseText);
+        }
+    });
+}
